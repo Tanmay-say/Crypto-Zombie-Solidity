9803e0edd9a8eb7cdf219eec94d573572f70f519
TanmaySayareLenovo <144255204+Tanmay-say@users.noreply.github.com>
2025-12-06 00:34:29 +0530

    contract.sol Chapter 4: Gas and cluster identical data types together

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -1,17 +1,20 @@
 pragma solidity >=0.5.0 <0.6.0;
 
+import "./ownable.sol";
 
+contract ZombieFactory is Ownable {
 
+    event NewZombie(uint zombieId, string name, uint dna);
 
-contract ZombieFactory{
-    uint dnaDigit = 16;
-    uint dnaModulus = 10 ** dnaDigit;
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
 
-    event NewZombie(uint zombieId, uint dna, string name);
-
-    struct Zombie{
+    struct Zombie {
         string name;
         uint dna;
+        uint32 level;
+        uint32 readyTime;
+        // Add new data here
     }
 
     Zombie[] public zombies;
@@ -19,26 +22,23 @@ contract ZombieFactory{
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
 
-// internal is the same as private, except that it's also accessible to contracts that inherit from this contract. (Hey, that sounds like what we want here!).
-//
-//external is similar to public, except that these functions can ONLY be called outside the contract — they can't be called by other functions inside that contract. We'll talk about why you might want to use external vs public later.
-
-    function _createZombie(string _name, uint _dna) internal  {  
+    function _createZombie(string memory _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        zombieToOwner[id] = msg.sender; // In Solidity, there are certain global variables that are available to all functions. One of these is msg.sender, which refers to the address of the person (or smart contract) who called the current function.
+        zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
-        emit NewZombie(id,_dna,_name);
+        emit NewZombie(id, _name, _dna);
     }
 
-    function _generateRandomDna(string _str) private view returns(uint) {
-        uint rand = uint(keccak256(abi.encodePacked(_str))) ;
-        return rand % dnaModulus ;
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
     }
 
-    function createRandomZombie(string _name) public{
-        require(ownerZombieCount[msg.sender] == 0); // require to make sure this function only gets executed one time per user, when they create their first zombie.
+    function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
-    } 
-}
+    }
 
+}
